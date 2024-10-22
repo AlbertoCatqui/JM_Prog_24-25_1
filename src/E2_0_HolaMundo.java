86a1648317e039bbe41d642b5033a4c2dcc7b7b1
AlbertoCatqui <alberto.catqui@iesjulianmarias.es>
2024-10-22 08:32:10 +0200

    Update E2_0_HolaMundo.java

--- src/E2_0_HolaMundo.java
+++ src/E2_0_HolaMundo.java
@@ -1,5 +1,5 @@
 public class E2_0_HolaMundo {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo actualización");
     }
 }
