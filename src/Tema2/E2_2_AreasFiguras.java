2c20678016d5c52fea49900d47d8b5953e8ed303
alberto (Clase) <alberto.catqui@iesjulianmarias.es>
2024-10-25 13:21:50 +0200

    commit prueba 25/10

--- src/Tema2/E2_2_AreasFiguras.java
+++ src/Tema2/E2_2_AreasFiguras.java
@@ -7,7 +7,7 @@ public class E2_2_AreasFiguras {
         boolean out = true;
         Scanner sc = new Scanner(System.in);
         while (out == true) {
-            System.out.println("Elie que de que figura quieres calcular el área: 1 cuadrado | 2 Rectángulo | 3 Triángulo");
+            System.out.println("Elie que de que figura quieres calcular el área: 1 cuadrado | 2 Rectángulo | 3 Triángulo | 4 Círculo");
             int option = sc.nextInt();
             if (option == 1) {
                 System.out.println("Has elegido cuadrado");
@@ -21,7 +21,7 @@ public class E2_2_AreasFiguras {
                 int ladoR1 = sc.nextInt();
                 System.out.println("intoduce el segundo lado en cm:");
                 int ladoR2 = sc.nextInt();
-                System.out.println("El área del cuadrado es: " + (ladoR1 * ladoR2) + "cm²");
+                System.out.println("El área del rectángulo es: " + (ladoR1 * ladoR2) + "cm²");
                 out = false;
             } else if (option == 3) {
                 System.out.println("Has elegido triángulo");
@@ -29,7 +29,13 @@ public class E2_2_AreasFiguras {
                 int baseT = sc.nextInt();
                 System.out.println("intoduce la altura en cm:");
                 int HeightT = sc.nextInt();
-                System.out.println("El área del cuadrado es: " + ((baseT * HeightT) / 2) + "cm²");
+                System.out.println("El área del triángulo es: " + ((baseT * HeightT) / 2) + "cm²");
+                out = false;
+            } else if (option == 4) {
+                System.out.println("Has elegido círculo");
+                System.out.println("intoduce el radio en cm:");
+                double radiusC = sc.nextDouble();
+                System.out.println("El área del círculo es: " + (radiusC * radiusC) * Math.PI + "cm²");
                 out = false;
             } else {
                 System.out.println("Número equivocado.");
