0eba719d935decf5bc1646098612f6e576d3d0ec
alberto (Clase) <alberto.catqui@iesjulianmarias.es>
2024-11-04 12:21:57 +0100

    commit 3 29/10

--- src/E2_0_HolaMundo.java
+++ src/E2_0_HolaMundo.java
@@ -1,57 +1,22 @@
+import javax.swing.*;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class E2_0_HolaMundo {
     public static void main(String[] args) {
-        /*
-        System.out.println("Hola Mundo actualización");
-        byte entero1 = 1 ;
-        short entero2 = 2;
-        int entero3 = 3;
-        long entero4 = 4;
-        float decimalSimple = 5;
-        double decimalDoble = 6;
-        char caracter = 'a';
-        String cadena = "tal";
-        boolean truefalse = true;
-
-
-        System.out.println(entero1);
-        System.out.println(entero2);
-        System.out.println(entero3);
-        System.out.println(entero4);
-        System.out.println(decimalSimple);
-        System.out.println(decimalDoble);
-        System.out.println(caracter);
-        System.out.println(cadena);
-        System.out.println(truefalse);
-
-        String c1="Hola";
-        String c2="buenas";
-        String c3="tardes";
-        String c4="tal";
-        String c5="noseque";
-        System.out.println(c1+" "+c2+" "+c3+" "+c4+" "+c5);
+        /*JOptionPane.showMessageDialog(null, "holabuena tarde");
+        int num = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingresa un numero"));
+        JOptionPane.showMessageDialog(null, num);*/
+        Pattern pat = Pattern.compile( "^[\\w-]+(\\.[\\w-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
+        String cadena;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Introduce un número:");
-        int numero = sc.nextInt();
-        System.out.println(numero);
-         */
-        //Iguales
-        String cadena1 = "Comparar cadena";
-        String cadena2 = "Comparar cadena";
-        int compara = cadena1.compareTo(cadena2);
-        System.out.println(compara);
-
-        //diferentes
-        String cadena3 = "Comparar cadena";
-        String cadena4 = "COMPARAR";
-        int compara2 = cadena3.compareTo(cadena4);
-        System.out.println(compara2);
-
-        //Iguales ignorecase
-        String cadena5 = "COMPARAR CADENA";
-        String cadena6 = "Comparar cadena";
-        int compara3 = cadena5.compareToIgnoreCase(cadena6);
-        System.out.println(compara3);
+        cadena = sc.nextLine();
+        Matcher mat = pat.matcher(cadena);
+        if (mat.matches()) {
+            System.out.println("SI");
+        } else {
+            System.out.println("NO");
+        }
     }
 }
